b642d5208865d047d6ae7e599434b060a7a8cba4
Martin Andersen <marander@protonmail.com>
2020-07-14 15:56:07 +0200

    Update logger.js

--- src/pages/api/webhooks/logger.js
+++ src/pages/api/webhooks/logger.js
@@ -1,6 +1,17 @@
+import Pusher from 'pusher';
+
+const channels = new Pusher({
+  appId: process.env.PUSHER_APP_ID,
+  key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY,
+  secret: process.env.PUSHER_SECRET,
+  cluster: 'eu'
+});
+
 export default (req, res) => {
   console.log('webhook received', new Date());
   console.log(JSON.stringify(req.body, null, 3));
+  channels.trigger('webhooks', 'incoming-webhook', JSON.stringify(req.body));
 
   res.send('received');
+  res.send('webhook received');
 };
