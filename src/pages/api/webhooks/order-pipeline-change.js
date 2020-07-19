3bfbd611c90d4a58dd7c156071a07354f5b1d84a
Martin Andersen <martin.andersn@gmail.com>
2020-07-19 15:31:43 +0200

    let client, import local lib/api

--- src/pages/api/webhooks/order-pipeline-change.js
+++ src/pages/api/webhooks/order-pipeline-change.js
@@ -1,6 +1,6 @@
-//import { getClient } from 'lib-api/payment-providers/vipps';
+import { getClient } from 'lib-api/payment-providers/vipps';
 import { updateCrystallizeOrder } from 'lib-api/crystallize/order';
-import { getClient } from '@crystallize/node-vipps';
+
 export default async (req, res) => {
   const {
     orders: { get: order }
